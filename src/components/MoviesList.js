6c6e232a718945d34350a0ab007524d221e2fa49
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2020-07-31 21:16:23 -0700

    se agrego MoviesList component

---
+++ src/components/MoviesList.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import PropType from "prop-types";
+import { Movie } from "./Movie";
+
+export class MoviesList extends Component{
+
+    static PropType={
+        movies: PropType.array
+    }
+
+    render(){   
+        const { movies } = this.props
+        return(
+
+     <div className="MoviesList">
+            { movies.map(movie => {
+        
+        return(
+            <div key={movie.imdbID} className="MoviesList-item">
+        
+            <Movie 
+        title={movie.Title}
+        year={movie.Year}
+        poster={movie.Poster} 
+        />
+
+        </div>
+             )
+        })
+    }
+        </div>
+    )    
+  }
+}
