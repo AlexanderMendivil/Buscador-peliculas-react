6c57f84b807c85696341bbed259077f2158b1a03
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2020-08-01 17:51:28 -0700

    se agrego componente Home

---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+import React, {Component} from "react";
+import { Title } from "../components/title";
+import { SearchForm } from "../components/SearchForm";
+import { MoviesList } from "../components/MoviesList";
+
+export class Home extends Component{
+   
+
+    state ={ 
+        results: [],
+        usedSearch: false
+        }
+    
+    handleResults = ( results ) =>{
+      this.setState({ results, usedSearch:true })
+    }
+
+    renderResults(){
+       return this.state.results.length === 0 
+        ? <p>Sorry, Results not found</p>
+        : <MoviesList movies={this.state.results} />
+        
+    }
+   
+    render(){
+        return(            
+        <div >
+        <Title>Search Movies</Title>
+        <div className="SearchForm-wrapper">
+        <SearchForm onResults={this.handleResults} />
+        </div>
+        {this.state.usedSearch 
+        ? this.renderResults()
+        : <small>Use the form to search a movie </small>}
+      </div>
+        )
+    }
+}
